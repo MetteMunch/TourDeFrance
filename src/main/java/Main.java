403d2d749dda490596d9187b322ea917faa7aa37
MetteMunch <meha0005@stud.kea.dk>
2024-08-29 17:25:52 +0200

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Statistics st = new Statistics("src/main/resources/tdffinishers2024.csv");
 
-        System.out.println(st.getTeams());
+        //System.out.println(st.getFullList());
+        System.out.println(st.getTeamsWithMembers());
+        //System.out.println(st.getTeamMembers("lidl-trek"));
     }
 }
--- src/main/java/Statistics.java
+++ src/main/java/Statistics.java
@@ -5,26 +5,53 @@ public class Statistics {
     private List<Rider> fullList;
 
 
-    public Statistics(String fileName){
+    public Statistics(String fileName) {
         TDFFileReader tdf = new TDFFileReader(fileName);
         fullList = tdf.readFile();
     }
 
-    public Set<String> getTeams(){
+    public Set<String> getTeams() {
         Set<String> result = new HashSet<>();
-        for(Rider r : fullList){
+        for (Rider r : fullList) {
             result.add(r.getTeam());
         }
         return result;
     }
 
-//    public List<Rider> getTeamsWithMembers(){
-//        //TO DO lav kode
-//    }
+    public Map<String, List<String>> getTeamsWithMembers() {
+        Map<String, List<String>> returnList = new TreeMap<>();
+        String teamName = "";
+        String riderName = "";
 
-//    public Map<String,String[]> getTeamMembers(String team){
-//        //TO DO lav kode
-//    }
+        for (Rider r : fullList) {
+            teamName = r.getTeam();
+            riderName = r.getFullName();
+            //først tjekker vi om vores Map allerede indeholder Key, hvis ikke så tilføjer vi denne med en tom ArrayList.
+            if(!returnList.containsKey(teamName)){
+                returnList.put(teamName,new ArrayList<>());
+            }
+            //efter at Key er oprettet, så bruger vi denne til at tilføje navn i ArrayList.
+            returnList.get(teamName).add(riderName);
 
+        }
+        return returnList;
+    }
 
+    public String getTeamMembers(String team) {
+        String result = "";
+
+        for (Rider r : fullList) {
+            if (r.getTeam().equalsIgnoreCase(team)) {
+                result += "\n"+r.getFullName();
+            }
+        }
+        return "Team: "+team+", have following riders: "+result;
+    }
+
+    public List<Rider> getFullList() {
+        return fullList;
+    }
 }
+
+
+
